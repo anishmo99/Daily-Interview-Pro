cb4ec89a3251396abc6c22774853713849e273d6
anishmo99 <ani10sh@gmail.com>
2020-06-09 10:50:35 +0530

    Create floorCeilingInBST.py

---
+++ floorCeilingInBST.py
@@ -0,0 +1,32 @@
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+
+def findCeilingFloor(root, key, floor=None, ceil=None):
+    
+    if root is None:
+        return floor,ceil
+    
+    if root.value==key:
+        return root.value,root.value
+    
+    elif key<root.value:
+        return findCeilingFloor(root.left,key,floor,root.value)
+    
+    else:
+        return findCeilingFloor(root.right,key,root.value,ceil)
+
+root = Node(8)
+root.left = Node(4)
+root.right = Node(12)
+  
+root.left.left = Node(2)
+root.left.right = Node(6)
+  
+root.right.left = Node(10)
+root.right.right = Node(14)
+
+print findCeilingFloor(root, 5)
+# (4, 6)
