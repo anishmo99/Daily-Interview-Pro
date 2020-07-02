cc5be3f95fafc9dd3f54a3cb606099fc740c86e9
anishmo99 <ani10sh@gmail.com>
2020-07-02 11:42:45 +0530

    update .gitignore

--- test.py
+++ test.py
@@ -1,50 +1,5 @@
-class ListNode(object):
-  def __init__(self, x):
-    self.val = x
-    self.next = None
-  
-  # Function to print the list
-  def printList(self):
-    node = self
-    output = ''
-    while node != None:
-      output += str(node.val)
-      output += " "
-      node = node.next
-    print(output)
+def max_subarray_sum(arr):
+  # Fill this in.
 
-  # Iterative Solution
-  def reverseIteratively(self, head):
-    cur = head
-    prev = None
-    while cur is not None:
-        next=cur.next
-        cur.next=prev
-        prev=cur
-        cur=next
-    self.head=prev
-
-  # Recursive Solution
-  def reverseRecursively(self, head):
-    # Implement this.
-
-# Test Program
-# Initialize the test list:
-testHead = ListNode(4)
-node1 = ListNode(3)
-testHead.next = node1
-node2 = ListNode(2)
-node1.next = node2
-node3 = ListNode(1)
-node2.next = node3
-testTail = ListNode(0)
-node3.next = testTail
-
-print("Initial list: ")
-testHead.printList()
-# 4 3 2 1 0
-testHead.reverseIteratively(testHead)
-#testHead.reverseRecursively(testHead)
-print("List after reversal: ")
-testTail.printList()
-# 0 1 2 3 4
+print max_subarray_sum([34, -50, 42, 14, -5, 86])
+# 137
