48c4f57a5157456641e9766a2c7b898e86c15690
anishmo99 <ani10sh@gmail.com>
2020-07-16 22:45:41 +0530

    Create 3Sum.cpp

---
+++ 3Sum.cpp
@@ -0,0 +1,23 @@
+/* You are required to complete the function below
+*  arr[]: input array
+*  n: size of array
+*/
+bool findTriplets(int arr[], int n)
+{
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        int l = i + 1;
+        int r = n - 1;
+        while (l < r)
+        {
+            if (arr[i] + arr[l] + arr[r] == 0)
+                return true;
+            else if (arr[i] + arr[l] + arr[r] > 0)
+                r--;
+            else
+                l++;
+        }
+    }
+    return false;
+}
