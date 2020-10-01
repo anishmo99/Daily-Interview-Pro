02b1c2bf5d0bd02626cbe05769d90f143f9789c3
anishmo99 <ani10sh@gmail.com>
2020-10-02 00:00:52 +0530

    Update roomScheduling.cpp

--- roomScheduling.cpp
+++ roomScheduling.cpp
@@ -5,8 +5,11 @@ bool comp(vector<int> a, vector<int> b)
 int Solution::solve(vector<vector<int>> &A)
 {
     sort(A.begin(), A.end(), comp);
+
     priority_queue<int, vector<int>, greater<int>> pq;
+
     pq.push(A[0][1]);
+
     for (int i = 1; i < A.size(); i++)
     {
         int top = pq.top();
