f118eaf90755d79e33b33495a76dea018ab5385c
anishmo99 <ani10sh@gmail.com>
2020-10-05 10:23:49 +0530

    Create uniqueBST.cpp

---
+++ uniqueBST.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int numTrees(int n)
+    {
+        vector<int> dp(n + 1, 0);
+
+        dp[0] = dp[1] = 1;
+
+        // loop for number of keys (ith loop)
+        for (int i = 2; i <= n; i++)
+        {
+            // loop for number of combinations (jth loop)
+            for (int j = 0; j < i; j++)
+            {
+                // [number of keys to the left * number of keys to the right
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+};
