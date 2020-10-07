7fdd2f22d14015de6c0778899e2ff652f205d65e
anishmo99 <ani10sh@gmail.com>
2020-10-07 15:09:28 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,71 +1,53 @@
-struct TrieNode
+class Solution
 {
-    TrieNode* child[26];
-    bool unique;
-
-    TrieNode()
+public:
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
     {
-        unique = true;
-        for (int i = 0; i < 26; i++)
-            child[i] = nullptr;
+        vector<int> temp;
+
+        if (matrix.empty())
+            return temp;
+
+        int top = 0, down = matrix.size() - 1, left = 0, right = matrix[0].size() - 1;
+        int dir = 0;
+
+        while (top <= down and left <= right)
+        {
+            if (dir == 0)
+            {
+                for (int i = left; i <= right; i++)
+                {
+                    temp.push_back(matrix[top][i]);
+                }
+                top++;
+            }
+            else if (dir == 1)
+            {
+                for (int i = top; i <= down; i++)
+                {
+                    temp.push_back(matrix[i][right]);
+                }
+                right--;
+            }
+            else if (dir == 2)
+            {
+                for (int i = right; i >= left; i--)
+                {
+                    temp.push_back(matrix[down][i]);
+                }
+                down--;
+            }
+            else if (dir == 3)
+            {
+                for (int i = down; i >= top; i--)
+                {
+                    temp.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+
+            dir = (dir + 1) % 4;
+        }
+        return temp;
     }
 };
-
-void makeTrie(TrieNode *root, string s)
-{
-    TrieNode *cur = root;
-
-    int i = 0;
-    while (i < s.size())
-    {
-        if (cur->child[s[i] - 'a'] == nullptr){
-            TrieNode* node = new TrieNode();
-            cur->child[s[i] - 'a'] = node;
-        }
-        else
-            cur->child[s[i] - 'a']->unique = false;
-
-        cur = cur->child[s[i] - 'a'];
-        i++;
-    }
-}
-
-string findPrefix(TrieNode *root, string s)
-{
-    if (s.empty())
-        return "";
-
-    string temp;
-
-    TrieNode *cur = root;
-
-    int i = 0;
-
-    do
-    {
-        cur = cur->child[s[i] - 'a'];
-        temp.push_back(s[i]);
-        i++;
-    } while (i < s.size() and !cur->unique);
-
-    return temp;
-}
-
-vector<string> Solution::prefix(vector<string> &A)
-{
-    TrieNode *root = new TrieNode();
-
-    vector<string> sol;
-
-    for (int i = 0; i < A.size(); i++)
-    {
-        makeTrie(root, A[i]);
-    }
-
-    for (int i = 0; i < A.size(); i++)
-    {
-        sol.push_back(findPrefix(root, A[i]));
-    }
-
-    return sol;
-}
